ae7b70a27df0a52a66bdb596dc8d07d18d6a7e2e
Jefferson <jeffmonteiro61@icloud.com>
2024-01-12 14:22:52 -0300

    just-adjustmentd

--- src/Post.jsx
+++ src/Post.jsx
@@ -2,9 +2,9 @@
 
 export function Post(props) {
     return (
-        <>
+        <div>
             <strong>{props.author}</strong>
             <p>{props.content}</p>
-        </>
+        </div>
     )
 }
